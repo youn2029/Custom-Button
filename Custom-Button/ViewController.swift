2cf219965e7836d0674c5c9d7986e76faae704b4
youn2029 <cocoa8090@gmail.com>
2019-04-07 21:38:25 +0900

    UIButton의 type과 위치, 크기, 타이틀을 설정하여 루트 뷰에 추가

--- Custom-Button/ViewController.swift
+++ Custom-Button/ViewController.swift
@@ -13,6 +13,14 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+        // 버튼 인스턴스를 생성하고, 속성을 설정한다.
+        let btn = UIButton(type: UIButton.ButtonType.system)          // ButtonType을 매개변수로 UIButton 선언
+        btn.frame = CGRect(x: 50, y: 100, width: 150, height: 30)     // 크기와 위치
+        btn.setTitle("테스트 버튼", for: .normal)                        // 타이틀 (상태랑 같이 정의)
+        
+        // 루트 뷰에 버튼 추가
+        self.view.addSubview(btn)
     }
 
 
