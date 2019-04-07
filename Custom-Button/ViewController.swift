d68a293b95af4a180ea9ee2c7ab9becdbf07d80c
youn2029 <cocoa8090@gmail.com>
2019-04-08 00:02:07 +0900

    UILabel를 프로그래밍 방식으로 추가

--- Custom-Button/ViewController.swift
+++ Custom-Button/ViewController.swift
@@ -9,21 +9,33 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    var subject : UILabel!   // 레이블에 해당되는 변수
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
         
+        // 레이블 인스턴스를 초기화
+        self.subject = UILabel()
+        self.subject.frame = CGRect(x: 0, y: 100, width: 150, height: 30)
+        self.subject.text = "Hello, Label~!!"
+        self.subject.textAlignment = NSTextAlignment(CTTextAlignment.center)    // 레이블 텍스트 정령
+        
         // 버튼 인스턴스를 생성하고, 속성을 설정한다.
         let btn = UIButton(type: UIButton.ButtonType.system)          // ButtonType을 매개변수로 UIButton 선언
-        btn.frame = CGRect(x: 50, y: 100, width: 150, height: 30)     // 크기와 위치
+        btn.frame = CGRect(x: 50, y: 200, width: 150, height: 30)     // 크기와 위치
         btn.setTitle("테스트 버튼", for: .normal)                        // 타이틀 (상태랑 같이 정의)
         
         // 버튼을 수평 중앙 정령
-        btn.center = CGPoint(x: self.view.frame.width / 2 , y: 100)
+        btn.center = CGPoint(x: self.view.frame.width / 2 , y: 200)
+        
+        // 레이블을 수평 중앙 정력
+        self.subject.center = CGPoint(x: self.view.frame.width / 2, y: 100)
         
         // 루트 뷰에 버튼 추가
         self.view.addSubview(btn)
+        self.view.addSubview(subject)
         
         // 버튼의 이벤트와 메소드 btnOnClick(_:)을 연결한다.
         // _ target : 호출 할 메소드가 있는 인스턴스를 가르키는 매개변수
@@ -36,6 +48,7 @@ class ViewController: UIViewController {
         // 호출한 객체가 버튼이면
         if let btn = sender as? UIButton {
             btn.setTitle("클릭되었습니다.", for: .normal)
+            self.subject.text = "GoodBye, Label~~!"
         }
     }
 
